8410c996e8287a6cb2e979b58287b6620dfd4f12
agakhansr <agaxanist@gmail.com>
2024-08-22 19:28:16 +0400

    create request done

--- src/pages/dashboard/customer/fiziki/requests/new.tsx
+++ src/pages/dashboard/customer/fiziki/requests/new.tsx
@@ -2,11 +2,11 @@ import { Helmet } from 'react-helmet-async';
 
 import { CONFIG } from 'src/config-global';
 
-import { CreditCreateView } from 'src/sections/credits/view';
+import { CreditCreateView } from 'src/sections/request/view';
 
 // ----------------------------------------------------------------------
 
-const metadata = { title: `Create a new Credit | Dashboard - ${CONFIG.site.name}` };
+const metadata = { title: `Yeni Sorğu yarat | Dashboard - ${CONFIG.site.name}` };
 
 export default function Page() {
   return (
