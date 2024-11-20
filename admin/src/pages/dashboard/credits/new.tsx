f6a44f2fdab0012bea0942838c7b74161984ad2e
agakhansr <agaxanist@gmail.com>
2024-11-20 16:39:45 +0400

    unnecessary link removed from main nav

--- admin/src/pages/dashboard/credits/new.tsx
+++ admin/src/pages/dashboard/credits/new.tsx
@@ -6,7 +6,7 @@ import { CreditCreateView } from 'src/sections/credits/view';
 
 // ----------------------------------------------------------------------
 
-const metadata = { title: `Create a new Credit | Dashboard - ${CONFIG.site.name}` };
+const metadata = { title: `Kredit yarat | Dashboard - ${CONFIG.site.name}` };
 
 export default function Page() {
   return (
