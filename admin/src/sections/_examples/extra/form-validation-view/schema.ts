3c666eea4f8c71d276c1f494b0e6cba36664d488
agakhansr <agaxanist@gmail.com>
2024-11-30 15:05:44 +0400

    minor changes

--- admin/src/sections/_examples/extra/form-validation-view/schema.ts
+++ admin/src/sections/_examples/extra/form-validation-view/schema.ts
@@ -77,3 +77,126 @@ export const FormSchema = zod
     message: 'End date cannot be earlier than start date!',
     path: ['endDate'],
   });
+
+  export const creditSchema = zod
+  .object({
+    fin: zod
+      .string()
+      .min(7, { message: 'Fin 7 simvoldan az ola bilməz!' })
+      .max(7, { message: 'Fini 7 simvoldan çox ola bilməz!' }),
+    serialNumber: zod
+      .string()
+      .max(11, { message: 'Ş/V seriyası 11 simvoldan çox ola bilməz!' })
+      .min(9, {
+        message: 'Ş/V seriyası və nömrəsi 9 simvoldan az ola bilməz!',
+      }),
+    passportStatus: zod.string().min(1, { message: 'Vəsiqənin statusu tələb olunur!' }),
+    name: zod.string().min(1, { message: 'Ad tələb olunur!' }),
+    surname: zod.string().min(1, { message: 'Soyad tələb olunur!' }),
+    fatherName: zod.string().min(1, { message: 'Ata adı tələb olunur!' }),
+    born: zod.string().min(1, { message: 'Doğum tarixi tələb olunur!' }),
+    familyRelationship: zod.string().min(1, { message: 'Ailə vəziyyəti tələb olunur!' }),
+    gender: zod.string().min(1, { message: 'Cinsiyyət tələb olunur!' }),
+    state: zod.string().min(1, { message: 'Doğum yeri tələb olunur!' }),
+    zipCode: zod.string().min(1, { message: 'Zip kodu tələb olunur!' }),
+    role: zod.string().min(1, { message: 'Rol tələb olunur!' }),
+    email: zod
+      .string()
+      .min(1, { message: 'Email tələb olunur!' })
+      .email({ message: 'Email Adresini düzgün daxil edin!' }),
+    phoneNumber: zod.string().min(1, { message: 'Telefon nömrəsi tələb olunur!' }),
+    address: zod.string().min(1, { message: 'Yaşayış yeri tələb olunur!' }),
+    country: zod.string().min(1, { message: 'Ölkə tələb olunur!' }),
+    status: zod.string(),
+    reportNum: zod.string().min(1, { message: 'Hesabat nömrəsi tələb olunur!' }),
+    dateMade: zod.string().min(1, { message: 'Hesabatın yaradıldığı tarix tələb olunur!' }),
+    historyMadeDate: zod.string().min(1, { message: 'Tarixçənin açıldığı tarix tələb olunur!' }),
+    loanerId: zod.string().min(1, { message: 'Borcalanın ID-si tələb olunur!' }),
+    loanerScore: zod.number().min(1, { message: 'Borcalanın skoru tələb olunur!' }),
+    loanerAdd: zod.string().min(1, { message: 'Borcalanın ünvanı tələb olunur!' }),
+    loanerBornAdd: zod.string().min(1, { message: 'Borcalanın doğum yeri tələb olunur!' }),
+    loanerBornDate: zod.string().min(1, { message: 'Borcalanın doğum tarixi tələb olunur!' }),
+    education: zod.string().min(1, { message: 'Təhsil tələb olunur!' }),
+    workplaceName: zod.string().min(1, { message: 'İş yeri adı tələb olunur!' }),
+    workplaceAddress: zod.string().min(1, { message: 'İş yeri ünvanı tələb olunur!' }),
+    positionAndExperience: zod.string().min(1, { message: 'Vəzifə və təcrübə tələb olunur!' }),
+    monthlySalary: zod.string().min(1, { message: 'Aylıq maaş tələb olunur!' }),
+    totalMonthlyIncome: zod.string().min(1, { message: 'Cəmi aylıq gəlir tələb olunur!' }),
+    totalExpenses: zod.string().min(1, { message: 'Cəmi xərclər tələb olunur!' }),
+    netIncome: zod.string().min(1, { message: 'Net gəlir tələb olunur!' }),
+    contractStartDate: zod.string().min(1, { message: 'Müqavilənin başlama tarixi tələb olunur!' }),
+    contractEndDate: zod.string().min(1, { message: 'Müqavilənin bitmə tarixi tələb olunur!' }),
+    monthlySalaryAmount: zod.string().min(1, { message: 'Aylıq maaş məbləği tələb olunur!' }),
+    akbInfo: zod.string().min(1, { message: 'AKB məlumatı tələb olunur!' }),
+    internalRiskSystem: zod.string().min(1, { message: 'Daxili risk sistemi tələb olunur!' }),
+    propertyType: zod.string().min(1, { message: 'Əmlak növü tələb olunur!' }),
+    registrationNumber: zod.string().min(1, { message: 'Qeydiyyat nömrəsi tələb olunur!' }),
+    occupancyAddress: zod.string().min(1, { message: 'Ünvanı tələb olunur!' }),
+    ownershipStatus: zod.string().min(1, { message: 'Sahiblik statusu tələb olunur!' }),
+    numberOfRooms: zod.string().min(1, { message: 'Otaq sayı tələb olunur!' }),
+    area: zod.string().min(1, { message: 'Sahə tələb olunur!' }),
+    constructionYear: zod.string().min(1, { message: 'İnşa tarixi tələb olunur!' }),
+    marketValue: zod.string().min(1, { message: 'Bazar dəyəri tələb olunur!' }),
+    mortgageStatus: zod.string().min(1, { message: 'İpoteka statusu tələb olunur!' }),
+    monthlyRent: zod.string().min(1, { message: 'Aylıq icarə məbləği tələb olunur!' }),
+    avatarUrl: zod.string(),
+    vehicleData: zod
+      .object({
+        vehicleBrand: zod.string().min(1, { message: 'Marka tələb olunur!' }),
+        vehicleModel: zod.string().min(1, { message: 'Model tələb olunur!' }),
+        vehicleYear: zod.string().min(1, { message: 'İl tələb olunur!' }),
+        vehicleMarketValue: zod.string().min(1, { message: 'Bazar dəyəri tələb olunur!' }),
+        vehicleVin: zod.string().min(1, { message: 'VIN nömrəsi tələb olunur!' }),
+        vehicleNumber: zod.string().min(1, { message: 'Nömrə tələb olunur!' }),
+      })
+      .optional(),
+    zaminData: zod
+      .object({
+        name: zod.string().min(1, { message: 'Ad tələb olunur!' }),
+        surname: zod.string().min(1, { message: 'Soyad tələb olunur!' }),
+        fin: zod.string().min(1, { message: 'Fin tələb olunur!' }),
+        serialNumber: zod.string().min(1, { message: 'Ş/V seriyası tələb olunur!' }),
+        passportStatus: zod.string().min(1, { message: 'Vəsiqənin statusu tələb olunur!' }),
+        identityCard: zod.string().min(1, { message: 'Ş/V nömrəsi tələb olunur!' }),
+        issuedBy: zod.string().min(1, { message: 'Verən orqan tələb olunur!' }),
+        borrowerInfo: zod.string().min(1, { message: 'Borcalanın məlumatı tələb olunur!' }),
+        registrationAddress: zod.string().min(1, { message: 'Qeydiyyat ünvanı tələb olunur!' }),
+        residentialAddress: zod.string().min(1, { message: 'Yaşayış ünvanı tələb olunur!' }),
+        phones: zod.string().min(1, { message: 'Telefon nömrəsi tələb olunur!' }),
+        newZaminName: zod.string().min(1, { message: 'Ad tələb olunur!' }),
+        newZaminSurname: zod.string().min(1, { message: 'Soyad tələb olunur!' }),
+        newZaminFin: zod.string().min(1, { message: 'Fin tələb olunur!' }),
+        newZaminSerialNumber: zod.string().min(1, { message: 'Ş/V seriyası tələb olunur!' }),
+        newZaminPassportStatus: zod.string().min(1, { message: 'Vəsiqənin statusu tələb olunur!' }),
+        newZaminIdentityCard: zod.string().min(1, { message: 'Ş/V nömrəsi tələb olunur!' }),
+        newZaminIssuedBy: zod.string().min(1, { message: 'Verən orqan tələb olunur!' }),
+        newZaminRegistrationAddress: zod
+          .string()
+          .min(1, { message: 'Qeydiyyat ünvanı tələb olunur!' }),
+        newZaminResidentialAddress: zod
+          .string()
+          .min(1, { message: 'Yaşayış ünvanı tələb olunur!' }),
+        newZaminPhones: zod.string().min(1, { message: 'Telefon nömrəsi tələb olunur!' }),
+      })
+      .optional(),
+    loanTotal: zod.number().min(1, { message: 'Kredit məbləği boş buraxıla bilməz!' }),
+    loanPercentagePerYear: zod
+      .number()
+      .min(1, { message: 'İllik faiz dərəcəsi boş buraxıla bilməz!' }),
+    payPerMonth: zod.number().min(1, { message: 'Aylıq ödəniş məbləği boş buraxıla bilməz!' }),
+    totalCredit: zod.number().min(1, { message: 'Cəmi kredit məbləği boş buraxıla bilməz!' }),
+    totalPercetange: zod.number().min(1, { message: 'Cəmi faiz dərəcəsi boş buraxıla bilməz!' }),
+    comissionDecide: zod.boolean(),
+    whereToGetSignature: zod
+      .string()
+      .min(1, { message: 'Müqavilənin əldə ediləcəyi vasitə(lər) tələb olunur!' }),
+    telegramUsernameToGet: zod
+      .string()
+      .min(1, { message: 'Telegram istifadəçi adı tələb olunur!' }),
+    whatsappNumberToGet: zod.string().min(1, { message: 'WhatsApp nömrəsi tələb olunur!' }),
+    emailToGet: zod.string().min(1, { message: 'Email tələb olunur!' }),
+  })
+  .refine((data) => data.telegramUsernameToGet || data.whatsappNumberToGet || data.emailToGet, {
+    message: 'At least one of Telegram Username, WhatsApp Number, or Email is required!',
+    path: ['telegramUsernameToGet', 'whatsappNumberToGet', 'emailToGet'],
+  });
--- admin/src/sections/credits/credit-new-edit-form.tsx
+++ admin/src/sections/credits/credit-new-edit-form.tsx
@@ -29,8 +29,6 @@ import { Form, Field } from 'src/components/hook-form';
 
 import { PRODUCT_GENDER_OPTIONS } from 'src/_mock';
 
-import { GridAddIcon } from '@mui/x-data-grid';
-
 import ScoreCard from './scoreBoard';
 import {
   _familyRelationshipOptions,
@@ -46,6 +44,7 @@ import {
   combinedHeaders,
   guarantorLoanData,
 } from './credit-data';
+import { creditSchema } from '../_examples/extra/form-validation-view/schema';
 
 export type ValuesType = {
   [year: string]: {
@@ -481,128 +480,7 @@ const users = [
     ],
   },
 ];
-export const schema = zod
-  .object({
-    fin: zod
-      .string()
-      .min(7, { message: 'Fin 7 simvoldan az ola bilməz!' })
-      .max(7, { message: 'Fini 7 simvoldan çox ola bilməz!' }),
-    serialNumber: zod
-      .string()
-      .max(11, { message: 'Ş/V seriyası 11 simvoldan çox ola bilməz!' })
-      .min(9, {
-        message: 'Ş/V seriyası və nömrəsi 9 simvoldan az ola bilməz!',
-      }),
-    passportStatus: zod.string().min(1, { message: 'Vəsiqənin statusu tələb olunur!' }),
-    name: zod.string().min(1, { message: 'Ad tələb olunur!' }),
-    surname: zod.string().min(1, { message: 'Soyad tələb olunur!' }),
-    fatherName: zod.string().min(1, { message: 'Ata adı tələb olunur!' }),
-    born: zod.string().min(1, { message: 'Doğum tarixi tələb olunur!' }),
-    familyRelationship: zod.string().min(1, { message: 'Ailə vəziyyəti tələb olunur!' }),
-    gender: zod.string().min(1, { message: 'Cinsiyyət tələb olunur!' }),
-    state: zod.string().min(1, { message: 'Doğum yeri tələb olunur!' }),
-    zipCode: zod.string().min(1, { message: 'Zip kodu tələb olunur!' }),
-    role: zod.string().min(1, { message: 'Rol tələb olunur!' }),
-    email: zod
-      .string()
-      .min(1, { message: 'Email tələb olunur!' })
-      .email({ message: 'Email Adresini düzgün daxil edin!' }),
-    phoneNumber: zod.string().min(1, { message: 'Telefon nömrəsi tələb olunur!' }),
-    address: zod.string().min(1, { message: 'Yaşayış yeri tələb olunur!' }),
-    country: zod.string().min(1, { message: 'Ölkə tələb olunur!' }),
-    status: zod.string(),
-    reportNum: zod.string().min(1, { message: 'Hesabat nömrəsi tələb olunur!' }),
-    dateMade: zod.string().min(1, { message: 'Hesabatın yaradıldığı tarix tələb olunur!' }),
-    historyMadeDate: zod.string().min(1, { message: 'Tarixçənin açıldığı tarix tələb olunur!' }),
-    loanerId: zod.string().min(1, { message: 'Borcalanın ID-si tələb olunur!' }),
-    loanerScore: zod.number().min(1, { message: 'Borcalanın skoru tələb olunur!' }),
-    loanerAdd: zod.string().min(1, { message: 'Borcalanın ünvanı tələb olunur!' }),
-    loanerBornAdd: zod.string().min(1, { message: 'Borcalanın doğum yeri tələb olunur!' }),
-    loanerBornDate: zod.string().min(1, { message: 'Borcalanın doğum tarixi tələb olunur!' }),
-    education: zod.string().min(1, { message: 'Təhsil tələb olunur!' }),
-    workplaceName: zod.string().min(1, { message: 'İş yeri adı tələb olunur!' }),
-    workplaceAddress: zod.string().min(1, { message: 'İş yeri ünvanı tələb olunur!' }),
-    positionAndExperience: zod.string().min(1, { message: 'Vəzifə və təcrübə tələb olunur!' }),
-    monthlySalary: zod.string().min(1, { message: 'Aylıq maaş tələb olunur!' }),
-    totalMonthlyIncome: zod.string().min(1, { message: 'Cəmi aylıq gəlir tələb olunur!' }),
-    totalExpenses: zod.string().min(1, { message: 'Cəmi xərclər tələb olunur!' }),
-    netIncome: zod.string().min(1, { message: 'Net gəlir tələb olunur!' }),
-    contractStartDate: zod.string().min(1, { message: 'Müqavilənin başlama tarixi tələb olunur!' }),
-    contractEndDate: zod.string().min(1, { message: 'Müqavilənin bitmə tarixi tələb olunur!' }),
-    monthlySalaryAmount: zod.string().min(1, { message: 'Aylıq maaş məbləği tələb olunur!' }),
-    akbInfo: zod.string().min(1, { message: 'AKB məlumatı tələb olunur!' }),
-    internalRiskSystem: zod.string().min(1, { message: 'Daxili risk sistemi tələb olunur!' }),
-    propertyType: zod.string().min(1, { message: 'Əmlak növü tələb olunur!' }),
-    registrationNumber: zod.string().min(1, { message: 'Qeydiyyat nömrəsi tələb olunur!' }),
-    occupancyAddress: zod.string().min(1, { message: 'Ünvanı tələb olunur!' }),
-    ownershipStatus: zod.string().min(1, { message: 'Sahiblik statusu tələb olunur!' }),
-    numberOfRooms: zod.string().min(1, { message: 'Otaq sayı tələb olunur!' }),
-    area: zod.string().min(1, { message: 'Sahə tələb olunur!' }),
-    constructionYear: zod.string().min(1, { message: 'İnşa tarixi tələb olunur!' }),
-    marketValue: zod.string().min(1, { message: 'Bazar dəyəri tələb olunur!' }),
-    mortgageStatus: zod.string().min(1, { message: 'İpoteka statusu tələb olunur!' }),
-    monthlyRent: zod.string().min(1, { message: 'Aylıq icarə məbləği tələb olunur!' }),
-    avatarUrl: zod.string(),
-    vehicleData: zod
-      .object({
-        vehicleBrand: zod.string().min(1, { message: 'Marka tələb olunur!' }),
-        vehicleModel: zod.string().min(1, { message: 'Model tələb olunur!' }),
-        vehicleYear: zod.string().min(1, { message: 'İl tələb olunur!' }),
-        vehicleMarketValue: zod.string().min(1, { message: 'Bazar dəyəri tələb olunur!' }),
-        vehicleVin: zod.string().min(1, { message: 'VIN nömrəsi tələb olunur!' }),
-        vehicleNumber: zod.string().min(1, { message: 'Nömrə tələb olunur!' }),
-      })
-      .optional(),
-    zaminData: zod
-      .object({
-        name: zod.string().min(1, { message: 'Ad tələb olunur!' }),
-        surname: zod.string().min(1, { message: 'Soyad tələb olunur!' }),
-        fin: zod.string().min(1, { message: 'Fin tələb olunur!' }),
-        serialNumber: zod.string().min(1, { message: 'Ş/V seriyası tələb olunur!' }),
-        passportStatus: zod.string().min(1, { message: 'Vəsiqənin statusu tələb olunur!' }),
-        identityCard: zod.string().min(1, { message: 'Ş/V nömrəsi tələb olunur!' }),
-        issuedBy: zod.string().min(1, { message: 'Verən orqan tələb olunur!' }),
-        borrowerInfo: zod.string().min(1, { message: 'Borcalanın məlumatı tələb olunur!' }),
-        registrationAddress: zod.string().min(1, { message: 'Qeydiyyat ünvanı tələb olunur!' }),
-        residentialAddress: zod.string().min(1, { message: 'Yaşayış ünvanı tələb olunur!' }),
-        phones: zod.string().min(1, { message: 'Telefon nömrəsi tələb olunur!' }),
-        newZaminName: zod.string().min(1, { message: 'Ad tələb olunur!' }),
-        newZaminSurname: zod.string().min(1, { message: 'Soyad tələb olunur!' }),
-        newZaminFin: zod.string().min(1, { message: 'Fin tələb olunur!' }),
-        newZaminSerialNumber: zod.string().min(1, { message: 'Ş/V seriyası tələb olunur!' }),
-        newZaminPassportStatus: zod.string().min(1, { message: 'Vəsiqənin statusu tələb olunur!' }),
-        newZaminIdentityCard: zod.string().min(1, { message: 'Ş/V nömrəsi tələb olunur!' }),
-        newZaminIssuedBy: zod.string().min(1, { message: 'Verən orqan tələb olunur!' }),
-        newZaminRegistrationAddress: zod
-          .string()
-          .min(1, { message: 'Qeydiyyat ünvanı tələb olunur!' }),
-        newZaminResidentialAddress: zod
-          .string()
-          .min(1, { message: 'Yaşayış ünvanı tələb olunur!' }),
-        newZaminPhones: zod.string().min(1, { message: 'Telefon nömrəsi tələb olunur!' }),
-      })
-      .optional(),
-    loanTotal: zod.number().min(1, { message: 'Kredit məbləği boş buraxıla bilməz!' }),
-    loanPercentagePerYear: zod
-      .number()
-      .min(1, { message: 'İllik faiz dərəcəsi boş buraxıla bilməz!' }),
-    payPerMonth: zod.number().min(1, { message: 'Aylıq ödəniş məbləği boş buraxıla bilməz!' }),
-    totalCredit: zod.number().min(1, { message: 'Cəmi kredit məbləği boş buraxıla bilməz!' }),
-    totalPercetange: zod.number().min(1, { message: 'Cəmi faiz dərəcəsi boş buraxıla bilməz!' }),
-    comissionDecide: zod.boolean(),
-    whereToGetSignature: zod
-      .string()
-      .min(1, { message: 'Müqavilənin əldə ediləcəyi vasitə(lər) tələb olunur!' }),
-    telegramUsernameToGet: zod
-      .string()
-      .min(1, { message: 'Telegram istifadəçi adı tələb olunur!' }),
-    whatsappNumberToGet: zod.string().min(1, { message: 'WhatsApp nömrəsi tələb olunur!' }),
-    emailToGet: zod.string().min(1, { message: 'Email tələb olunur!' }),
-  })
-  .refine((data) => data.telegramUsernameToGet || data.whatsappNumberToGet || data.emailToGet, {
-    message: 'At least one of Telegram Username, WhatsApp Number, or Email is required!',
-    path: ['telegramUsernameToGet', 'whatsappNumberToGet', 'emailToGet'],
-  });
+
 // const getBackgroundColor = (daysLate: any) => {
 //   if (daysLate === '-') return '#C6C6C6'; // No information (gray)
 //   if (daysLate === 0) return '#00B0F0'; // 0 days delay (blue)
@@ -624,7 +502,7 @@ export function CreateCreditForm() {
 
   const methods = useForm({
     mode: 'onSubmit',
-    resolver: zodResolver(schema),
+    resolver: zodResolver(creditSchema),
     defaultValues: {
       fin: '',
       serialNumber: '',
@@ -695,7 +573,7 @@ export function CreateCreditForm() {
     } catch (error) {
       toast.error(error.message);
     }
-  }
+  };
 
   const onSubmit = handleSubmit((data) => {
     console.log('Submitted Data:', data);
@@ -725,7 +603,7 @@ export function CreateCreditForm() {
 
     toast.success('Zamin əlavə edildi!');
   };
-  
+
   const addNewGuarantor = () => {
     setShowNewForm(true);
   };
@@ -973,23 +851,28 @@ export function CreateCreditForm() {
                   Zamin barəsində məlumatlar
                 </Typography>
                 <Card sx={{ p: 3 }}>
-              <Box
-                display="grid"
-                rowGap={2}
-                columnGap={2}
-                gridTemplateColumns={{ xs: '1fr', sm: '1fr 1fr' }}
-              >
-                <Field.Text name="serialNumber" label="Serial Number" required variant="outlined" />
-                <Field.Text name="fin" label="FIN" required variant="outlined" />
-              </Box>
-              <Button
-                onClick={handleSearchNewGuarantor}
-                variant="contained"
-                sx={{ mt: 2, width: '100%', backgroundColor: '#2D9CDB' }}
-              >
-                Axtar
-              </Button>
-            </Card>
+                  <Box
+                    display="grid"
+                    rowGap={2}
+                    columnGap={2}
+                    gridTemplateColumns={{ xs: '1fr', sm: '1fr 1fr' }}
+                  >
+                    <Field.Text
+                      name="serialNumber"
+                      label="Serial Number"
+                      required
+                      variant="outlined"
+                    />
+                    <Field.Text name="fin" label="FIN" required variant="outlined" />
+                  </Box>
+                  <Button
+                    onClick={handleSearchNewGuarantor}
+                    variant="contained"
+                    sx={{ mt: 2, width: '100%', backgroundColor: '#2D9CDB' }}
+                  >
+                    Axtar
+                  </Button>
+                </Card>
                 <Divider sx={{ mb: 3 }} />
                 {selectedUserGuarantorData.map((zamin: any) => (
                   <Box
@@ -998,7 +881,7 @@ export function CreateCreditForm() {
                     rowGap={3}
                     columnGap={2}
                     sx={{
-                      mb: 4
+                      mb: 4,
                     }}
                     gridTemplateColumns={{ xs: '128px 1fr', sm: '128px 1fr' }}
                   >
@@ -1244,13 +1127,13 @@ export function CreateCreditForm() {
                     }}
                     disabled
                   />
-                    <Button
-                      onClick={() => handleAddGuarantor(member.fields.fin)}
-                      variant="contained"
-                      sx={{ mt: 2, width: '100%', backgroundColor: '#2D9CDB' }}
-                    >
-                      Zamin et
-                    </Button>
+                  <Button
+                    onClick={() => handleAddGuarantor(member.fields.fin)}
+                    variant="contained"
+                    sx={{ mt: 2, width: '100%', backgroundColor: '#2D9CDB' }}
+                  >
+                    Zamin et
+                  </Button>
                 </Box>
                 <Grid xs={12} md={8} item>
                   <Card sx={{ p: 3 }}>
@@ -1266,12 +1149,7 @@ export function CreateCreditForm() {
                         columnGap={2}
                         gridTemplateColumns={{ xs: 'repeat(3, 1fr)' }}
                       >
-                        <Field.Text
-                          name="fin"
-                          label="Fin"
-                          value={member.fields.fin}
-                          disabled
-                        />
+                        <Field.Text name="fin" label="Fin" value={member.fields.fin} disabled />
                         <Field.Text
                           name="serialNumber"
                           label="Ş/V seriyası və nömrəsi"
@@ -1291,12 +1169,7 @@ export function CreateCreditForm() {
                         columnGap={2}
                         gridTemplateColumns={{ xs: 'repeat(2, 1fr)' }}
                       >
-                        <Field.Text
-                          name="name"
-                          label="Adı"
-                          value={member.fields.name}
-                          disabled
-                        />
+                        <Field.Text name="name" label="Adı" value={member.fields.name} disabled />
                         <Field.Text
                           name="surname"
                           label="Soyadı"
@@ -1450,11 +1323,16 @@ export function CreateCreditForm() {
           type="submit"
           variant="contained"
           disabled={isSubmitting}
+          sx={{
+            backgroundColor: 'black',
+            width: '300px',
+            color: 'white',
+          }}
           onClick={() => {
             console.log('Submitted Data:', methods.getValues());
           }}
         >
-          Submit
+          Təsdiqlə
         </Button>
       </Stack>
     </Form>
