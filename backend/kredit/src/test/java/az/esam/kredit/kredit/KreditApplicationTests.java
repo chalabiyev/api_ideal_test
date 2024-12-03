547e81c581d189fccb704c7a6397c94b98b0f743
Cihan <>
2024-12-03 11:02:47 +0300

    no comment

--- backend/kredit/src/test/java/az/esam/kredit/kredit/KreditApplicationTests.java
+++ backend/kredit/src/test/java/az/esam/kredit/kredit/KreditApplicationTests.java
@@ -3,7 +3,7 @@ package az.esam.kredit.kredit;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class KreditApplicationTests {
 
 	@Test
