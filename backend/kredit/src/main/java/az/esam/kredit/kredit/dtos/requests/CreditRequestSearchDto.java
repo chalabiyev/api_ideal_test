0f6d84ac26caa574c983fabf9da991a458b72d7a
Cihan Coşgun <ccosgun@tspb.org.tr>
2024-12-31 11:04:35 +0300

    new file:   admin_v2/src/types/CreditRequestSearchResponse.ts 	new file:   backend/kredit/src/main/java/az/esam/kredit/kredit/dtos/requests/CreditRequestSearchDto.java

---
+++ backend/kredit/src/main/java/az/esam/kredit/kredit/dtos/requests/CreditRequestSearchDto.java
@@ -0,0 +1,18 @@
+package az.esam.kredit.kredit.dtos.requests;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ *
+ * @author cihan
+ */
+@Data
+@Builder
+public class CreditRequestSearchDto {
+
+    private String creditType;
+    private String search;
+    private int pageSize;
+    private int page;
+}
