1492e8e264653672d0a1e94be52a19bcba4c89eb
aaphar <apharmagaramova@gmail.com>
2025-02-17 16:52:04 +0400

    credit type order bug fix

--- backend/kredit/src/main/java/az/esam/kredit/kredit/repositories/content_management/CreditTypeRepository.java
+++ backend/kredit/src/main/java/az/esam/kredit/kredit/repositories/content_management/CreditTypeRepository.java
@@ -3,5 +3,8 @@ package az.esam.kredit.kredit.repositories.content_management;
 import az.esam.kredit.kredit.entities.content_management.CreditType;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface CreditTypeRepository extends MongoRepository<CreditType, String> {
+    List<CreditType> findAllByOrderByCreatedDateDesc();
 }
