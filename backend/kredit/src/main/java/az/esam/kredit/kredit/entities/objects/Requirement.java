dd273477ab5dfc30afb51c1dd5464b4070618a6e
aaphar <apharmagaramova@gmail.com>
2025-02-18 16:01:43 +0400

    credit type patch problem fix

--- backend/kredit/src/main/java/az/esam/kredit/kredit/entities/objects/Requirement.java
+++ backend/kredit/src/main/java/az/esam/kredit/kredit/entities/objects/Requirement.java
@@ -1,10 +1,14 @@
 package az.esam.kredit.kredit.entities.objects;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Builder
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Requirement {
     private String age;
     private String documents;
